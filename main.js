ab66e0c3eb8ec104782a8c9aa145af4449b6ef3b
joaquin <crokkercook@hotmail.com>
2023-05-15 22:04:29 +0200

    Version1

---
+++ main.js
@@ -0,0 +1,58 @@
+
+// Togle navbar
+const menuIcon= document.querySelector("#menu-icon");
+const navbar= document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
+
+// Scroll Section
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 100;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id+"]").classList.add("active");
+            });
+            // activar seccion de animacion
+            sec.classList.add("show-animate");
+        }
+        // if para repetir animacion en scroll
+        else{
+            sec.classList.remove("show-animate");
+        }
+    });
+    // sticky header
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remover toggle icon y barra navegacion
+
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.remove("active");
+
+}
+
+// Formulario
+
+//  const $form = document.querySelector("#form")
+//  $form.addEventListener("submit", handleSubmit)
+
+//  function handleSubmit(event) {
+// // para que no se vaya a inicio
+// event.preventDefault()
+
+//  const form2= new FormData(this)
+//  console.log(form2.get("name"));
+//  }
